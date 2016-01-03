3232e27567d1eaa30377512bc91c790f60db71cf
khamul <charles_path7@hotmail.com>
2016-01-03 14:51:51 -0200

    Aula I do Tutorial How To Create MVC Application Step by Step In Visual Studio 2015(https://www.youtube.com/watch?v=FvZyRYzXyZI) Criado estrutra de pastas que pretendo usar até o fim do tutorial(e para outros)

---
+++ How To Create MVC Application Step by Step In Visual Studio 2015/Aula I/StudentDemo/StudentDemo/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentDemo.Models;
+
+namespace StudentDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Student(StudentModel Obj)
+        {
+            ViewBag.Msg = "Name " + Obj.Name + " Class " + Obj.Class + " Added";
+            return View();
+        }
+    }
+}
---
+++ How To Create MVC Application Step by Step In Visual Studio 2015/Aula I/StudentDemo/StudentDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentDemo.MvcApplication" Language="C#" %>
